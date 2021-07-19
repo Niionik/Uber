0d157085e651904ecdb44f2172c6bb207b753511
Nionik <Patrycja.pl.opalacz@gmail.com>
2021-07-19 06:43:20 +0200

    poprawki z weekendu

--- scri.js
+++ scri.js
@@ -1,4 +1,30 @@
 function hellotext() {
 
-    document.getElementsById('hung').innerHTML = "Zamów śniadanie, lunch i kolację.";
+    var x = Math.floor(Math.random() * 3);
+
+    if (x == 0) {
+        document.getElementById('hung').innerHTML = "Zamów śniadanie, lunch i kolację.";
+    } else if (x == 1) {
+        document.getElementById('hung').innerHTML = "Zamów.";
+    } else {
+        document.getElementById('hung').innerHTML = "Zamdfdów.";
+    }
+
+
+}
+
+
+function colbac() {
+
+    var x = Math.floor(Math.random() * 3);
+
+    if (x == 0) {
+        document.getElementById('back').style.background = "#FFF2D9";
+    } else if (x == 1) {
+        document.getElementById('back').style.background = "#FFD7D2";
+    } else {
+        document.getElementById('back').style.background = "#FFC043";
+    }
+
+
 }
