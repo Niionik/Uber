698de3de86305bf560c3ace868aa1044820704ab
Nionik <Patrycja.pl.opalacz@gmail.com>
2021-07-22 17:42:14 +0200

    dom 23

--- scri.js
+++ scri.js
@@ -13,9 +13,6 @@ function hellotext() {
     } else {
         document.getElementById('hung').innerHTML = "Zamów jedzenie z dostawą pod Twoje drzwi.";
     }
-
-
-
 }
 
 
@@ -23,67 +20,63 @@ function colbac() {
 
     var x = Math.floor(Math.random() * 3);
     var img = document.createElement("img");
-
-    img.setAttribute("src", "img/1.svg");
+    var backpagecolor = document.getElementById('wrapper');
+    var svg = ["img/1.svg", "img/2.svg", "img/3.svg", "img/4.svg"];
     img.classList.add("presentation");
 
     if (x == 0) {
-        document.getElementById('back').style.backgroundColor = "#FFF2D9";
-        document.getElementById("back").prepend(img);
-
+        img.setAttribute("src", svg[0]);
+        backpagecolor.style.backgroundColor = "#FFF2D9";
     } else if (x == 1) {
-        document.getElementById('back').style.backgroundColor = "#FFD7D2";
+        img.setAttribute("src", svg[1]);
+        backpagecolor.style.backgroundColor = "#FFD7D2";
+    } else if (x == 2) {
+        img.setAttribute("src", svg[3]);
+        backpagecolor.style.backgroundColor = "#FFC043";
     } else {
-        document.getElementById('back').style.backgroundColor = "#FFC043";
+        img.setAttribute("src", svg[2]);
+        backpagecolor.style.backgroundColor = "#FFF2D9";
     }
-
+    document.getElementById("back").prepend(img);
 }
+
+
+var menu = document.getElementById("headmenu");
 
 document.getElementById("bmenu").onclick = function() {
 
-    console.log(e);
-
     var menu = document.getElementById("headmenu");
 
     menu.style.display = (menu.style.display == "none") ? "block" : "none";
+
 }
-
-/*
-function menuclick() {
-
-    console.log(e);
-
-    var menu = document.getElementById("headmenu");
-
-    menu.style.display = (menu.style.display == "none") ? "block" : "none";
-}
-
-var but = document.querySelector(".menubuttonnav");
-
-but.addEventListener("click", menuclick(), false);
-
-
 
 function menuclick(e) {
+
     console.log(e);
 
     var menu = document.getElementById("headmenu");
 
-    menu.addEventListener("click", menu, false);
-
     menu.style.display = (menu.style.display == "none") ? "block" : "none";
 }
 
-function imgcreate() {
-    var img = document.createElement("img");
-    const back = document.getElementById('back');
-    img.classList.add("presentation");
+menu.addEventListener("click", menuclick);
 
 
-    if (back == style.backgroundColor = "#FFF2D9"){
-        img.setAttribute("src", "img/1.svg");
-    }else if (back == style.backgroundColor = "#FFD7D2"){
-        img.setAttribute("src", "img/2.svg");
+var boxstyle = document.getElementById("box");
+var body = document.getElementById("wrapper");
+
+
+function onScroll(e) {
+
+    console.log(body.scrollTop);
+
+    if (window.scrollY > 130) {
+        boxstyle.classList.add("findfixed");
+    } else if (window.scrollY == 0) {
+
+        boxstyle.classList.remove("findfixed");
     }
 }
-*/
+
+document.addEventListener('scroll', onScroll);
